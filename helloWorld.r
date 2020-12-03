7c006f5584d5ce40290536eb5266e6d9bdff522c
Tom <48026486+ddipT@users.noreply.github.com>
2020-12-03 18:14:39 +0000

    Added comments detailing strategy

--- helloWorld.r
+++ helloWorld.r
@@ -1,3 +1,7 @@
 print("Hello World")
+# To become familiar with R syntax I will write an example program, which takes input x+y where both x and y are integers
+# + can be any of the + - / * following, additon, subtraction, division or mulitplication
+# The program will then loop accepting new inputs constantly giving out the correct answer to the sum.
+
 
 
